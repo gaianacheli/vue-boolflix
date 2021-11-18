e6ca1734faa243459e77780fbfdf0b8be73b480e
gaianacheli <gaianacheli04@gmail.com>
2021-11-18 12:03:01 +0100

    inserito componente search

---
+++ src/components/SearchMovies.vue
@@ -0,0 +1,22 @@
+<template>
+  <div class="searchbox">
+    <input type="search" placeholder="Cerca Film">
+    <button class="btn">search</button>
+  </div>
+    
+</template>
+
+<script>
+export default {
+  name: 'SiteMain',
+  
+}
+</script>
+
+<!-- Add "scoped" attribute to limit CSS to this component only -->
+<style scoped lang="scss">
+.btn {
+  margin-left: 10px;
+}
+
+</style>
