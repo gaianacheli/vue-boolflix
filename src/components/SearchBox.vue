b45d376a096a2d3a4f1f1b639bf7856d94208b66
gaianacheli <gaianacheli04@gmail.com>
2021-11-18 17:07:20 +0100

    add components searchbox

---
+++ src/components/SearchBox.vue
@@ -0,0 +1,19 @@
+<template>
+    <div class="searchbox flex">
+      <input @keyup.enter="$emit('search',searchMovies)" class="bar" v-model="searchMovies" type="search" placeholder="Cerca un film">
+      <button  @click="$emit(`search`, searchMovies)">Search</button>
+
+    </div>
+</template>
+
+
+<script>
+export default {
+    
+    data(){
+        return{
+           searchMovies: "",  
+        }
+    },
+}
+</script>
--- src/components/SiteMain.vue
+++ src/components/SiteMain.vue
@@ -1,9 +1,6 @@
 <template>
  <div class="sitemain">
-   <div class="searchbox flex">
-      <input class="bar" v-model="searchMovies" type="search" placeholder="Cerca un film">
-      <button  @click="search()">Search</button>
-    </div>
+   <SearchBox @search='search'/>
     <div class="container_movies flex">
      <div class="movie flex" v-for="movie in movies" :key="movie.id">
         <Movie
@@ -19,24 +16,24 @@
 <script>
 import Movie from './Movie.vue'
 import axios from 'axios';
+import SearchBox from './SearchBox.vue';
 export default {
     components:{
         Movie,
+        SearchBox,
     },
     data(){
     return{
-        searchMovies: "",
         movies:[],
     }
   },
   methods:{
-    search(){
-       console.log(this.searchMovies);
-       this.callApi();
+    search(searchMovies){
+       this.callApi(searchMovies);
     
     },
-       callApi(){
-      let API_URL= "https://api.themoviedb.org/3/search/movie?api_key=4db7aa0aef6b2145477778558e1489b1&language=it-IT&query="+ this.searchMovies +"&page=1"
+       callApi(searchMovies){
+      let API_URL= "https://api.themoviedb.org/3/search/movie?api_key=4db7aa0aef6b2145477778558e1489b1&language=it-IT&query="+ searchMovies +"&page=1"
 
       axios
       .get(API_URL)
