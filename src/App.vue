8318c42bc610f4953fb0e03ccec61a50a63d5b77
gaianacheli <gaianacheli04@gmail.com>
2021-11-18 14:22:14 +0100

    add movie and style

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,71 @@
 <template>
   <div id="app">
-    
-    <SearchMovies />
-  </div>
+    <div class="searchbox flex">
+      <input class="bar" v-model="searchMovies" type="search" placeholder="Cerca un film">
+      <button  @click="search()">Search</button>
+    </div>
+    <div class="container_movies flex">
+     <div class="movie flex" v-for="movie in movies" :key="movie.id">
+        <Movie
+        :title="movie.title"
+        :original_title="movie.original_title"
+        :original_language="movie.original_language"
+        :vote_average="movie.vote_average"/>
+      </div>
+     </div>
+   </div>  
 </template>
 
 <script>
-import SearchMovies from './components/SearchMovies.vue'
+
+
+import axios from 'axios';
+import Movie from './components/Movie.vue'
 
 export default {
   name: 'App',
   components: {
-   SearchMovies
+    Movie,
+  },
+  data(){
+    return{
+       searchMovies: "",
+        movies:[],
+    }
+  },
+  methods:{
+    search(){
+      console.log(this.searchMovies);
+      this.callApi();
+      
+    },
+    callApi(){
+      let API_URL= "https://api.themoviedb.org/3/search/movie?api_key=4db7aa0aef6b2145477778558e1489b1&language=it-IT&query="+ this.searchMovies +"&page=1"
+
+      axios
+      .get(API_URL)
+      .then((response)=>{
+        this.movies=response.data.results;
+        console.log(this.movies);
+      })
+    },
   }
+  
 }
 </script>
 
 <style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+@import "./assets/scss/common.scss";
+.searchbox{
+  justify-content: center;
+  margin-top: 2rem;
 }
+.container_movies{
+ flex-wrap: wrap;
+ justify-content: space-around;
+}
+.movie {
+  padding: 10px;
+}
+
 </style>
---
+++ src/components/Movie.vue
@@ -0,0 +1,33 @@
+<template>
+     <div class="card flex">
+        <p> TITOLO: {{title}}</p>
+        <p>TITOLO ORIGINALE: {{original_title}}</p>
+        <p>LINGUA ORIGINAE: {{original_language}}</p>
+        <p>VOTO:{{vote_average}}</p>
+     </div>
+</template>
+
+
+<script>
+export default {
+    name:'Movie',
+    props: {
+        title: String,
+        original_title: String,
+        original_language: String,
+        vote_average: Number,
+
+    }
+
+}
+</script>
+
+<style scoped lang="scss">
+.card{
+    flex-direction: column;
+    justify-content: space-around;
+    border: 1px solid black;
+    padding: 1rem;
+}
+
+</style>
